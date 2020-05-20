43c7b8cd1e67a97eed4b3968687460a53bfbb062
Cynic4018 <q.katapol@gmail.com>
2020-05-21 02:55:13 +0700

    Ver. Demo

---
+++ Assets/Scripts/TouchActive.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchActive : MonoBehaviour
+{
+    Animator animator;
+    private float counter = 0f;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void OnMouseDown() {
+        animator.SetBool("Touch", true);
+        print("touch");
+        
+    }
+
+    void FixedUpdate() {
+        
+        if(animator.GetBool("Touch") == true){
+            counter += Time.deltaTime;
+            if(counter >= 0.15f){
+                counter = 0f;
+                animator.SetBool("Touch", false);
+            }
+        }
+    }
+}

a58e01baef0269b1c08f7ca299287339108cb42d
Poonnanun Poonnopathum <poonnanun1@gmail.com>
2021-01-22 02:26:39 +0700

    add tap to place

---
+++ Assets/Scripts/TabToPlaceObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TabToPlaceObject : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+    public Image image;
+    private Pose PlacementPose;
+    private ARRaycastManager aRRaycastManager;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        if(placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+    private void PlaceObject()
+    {
+        image.color = Color.red;
+        Instantiate(objectToPlace, placementIndicator.transform.position, placementIndicator.transform.rotation);
+    }
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        if(Camera.current != null)
+        {
+            var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+            var hits = new List<ARRaycastHit>();
+            aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+            placementPoseIsValid = hits.Count > 0;
+            if (placementPoseIsValid)
+            {
+                PlacementPose = hits[0].pose;
+            }
+        }
+    }
+}

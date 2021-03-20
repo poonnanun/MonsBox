ef84d6de1c96d3d350a1a4f38ebbc9929091cf75
Poonnanun Poonnopathum <poonnanun1@gmail.com>
2021-03-20 23:02:30 +0700

    Connected with database

---
+++ Assets/Scripts/DataStructure/DataStucture.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+#region Monster
+[System.Serializable]
+public class MonsterPool
+{
+    public int size;
+    public List<MonsterRawData> data;
+    public int GetSize()
+    {
+        return size;
+    }
+    public List<MonsterRawData> GetAllMonsters()
+    {
+        return data;
+    }
+    public MonsterRawData GetMonsterById(string id)
+    {
+        MonsterRawData a = data.Where(b => b.id == id).FirstOrDefault();
+        return a;
+    }
+    public MonsterRawData GetMonsterByNumber(int number)
+    {
+        MonsterRawData tmp = null;
+        if (number < data.Count())
+        {
+            tmp = data[number];
+        }
+        return tmp;
+    }
+    public override string ToString()
+    {
+        string output = "";
+        output += "------------------------\n";
+        output += data[0].ToString() + "\n";
+        //foreach (MonsterRawData a in data)
+        //{
+        //    output += a.ToString() + "\n";
+        //}
+        return output;
+    }
+}
+
+[System.Serializable]
+public class MonsterRawData
+{
+    public string id;
+    public string userId;
+    public string name;
+    public string asset;
+    public MonsterStausRawData status;
+    public string tier;
+
+    public override string ToString()
+    {
+        string output = "";
+        output += "id: " + id + " | ";
+        output += "userId: " + userId + " | ";
+        output += "name: " + name + " | ";
+        output += "asset: " + asset + " | ";
+        output += "tier: " + tier + " | ";
+        output += "Status{ " + status + " } ";
+        return output;
+    }
+}
+[System.Serializable]
+public class MonsterStausRawData
+{
+    public int hungry;
+    public int cleanliness;
+    public int happiness;
+    public int weight;
+    public int lastActivityTimestamp;
+
+    public override string ToString()
+    {
+        string output = "";
+        output += "Hungry: " + hungry + " | ";
+        output += "cleanliness: " + cleanliness + " | ";
+        output += "happiness: " + happiness + " | ";
+        output += "weight: " + weight + " | ";
+        output += "lastActivityTimestamp: " + lastActivityTimestamp + " | ";
+        return output;
+    }
+}
+
+#endregion
---
+++ Assets/Scripts/DataTestManger.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class DataTestManger : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI hungrinessText;
+    [SerializeField] private TextMeshProUGUI happinessText;
+    [SerializeField] private TextMeshProUGUI cleanlinessText;
+    [SerializeField] private Image monsterImage;
+    [SerializeField] private Button button1;
+    [SerializeField] private Button button2;
+    [SerializeField] private Sprite babyImage;
+
+    private string url;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetURL();
+        StartCoroutine(GetData());
+    }
+
+    private IEnumerator GetData()
+    {
+        string path = "monster";
+        Debug.Log("Start");
+        using (UnityWebRequest req = UnityWebRequest.Get(url+path))
+        {
+            yield return req.SendWebRequest();
+
+            if(req.isNetworkError || req.isHttpError)
+            {
+                Debug.Log(req.error + " ( " + (url+path) + " )");
+            }
+            else
+            {
+                Debug.Log(req.downloadHandler.text);
+                Debug.Log("Success");
+                MonsterPool tmp = JsonUtility.FromJson<MonsterPool>(req.downloadHandler.text);
+                Debug.Log(tmp.GetMonsterByNumber(0).ToString());
+                SetAllData(tmp.GetMonsterByNumber(1));
+            }
+            
+        }
+    }
+    public void SetAllData(MonsterRawData data)
+    {
+        //SetName(data.name);
+        SetName(data.name);
+        SetHungriness(data.status.hungry);
+        SetCleanliness(data.status.cleanliness);
+        SetHappiness(data.status.happiness);
+        SetImage(data.tier);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetName(string name)
+    {
+        nameText.text = name;
+    }
+    public void SetHungriness(int value)
+    {
+        hungrinessText.text = "Hungry: " + value.ToString();
+    }
+    public void SetCleanliness(int value)
+    {
+        cleanlinessText.text = "Cleanliness: " + value.ToString();
+    }
+    public void SetHappiness(int value)
+    {
+        happinessText.text = "Happiness: " + value.ToString();
+    }
+    public void SetImage(string value)
+    {
+        if(value == "BABY")
+        {
+            monsterImage.sprite = babyImage;
+        }
+    }
+    public void GetURL()
+    {
+        url = "http://192.168.5.150:8090/";
+    }
+}
